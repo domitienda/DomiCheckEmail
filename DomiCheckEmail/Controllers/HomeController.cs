7933ea8b757b661da75f39ef4d70d1802466577f
ioliver85 <ioliver85@gmail.com>
2013-06-19 10:29:07 +0200

    Subida del proyecto

---
+++ DomiCheckEmail/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using DomiCheckEmail.Models;
+
+namespace DomiCheckEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var modelo = new HomeModel();
+
+            return View(modelo);
+        }
+
+        public JsonResult EnviarMail(string servidor, string puerto, string usuario, string clave, string destinatario)
+        {
+            if(servidor == null || servidor.Equals(string.Empty))
+            {
+                return Json(new { success = false, log = "El campo servidor es obligatorio" }, "application/json");
+            }
+            if (puerto == null || puerto.Equals(string.Empty))
+            {
+                return Json(new { success = false, log = "El campo puerto es obligatorio" }, "application/json");
+            }
+            if (usuario == null || usuario.Equals(string.Empty))
+            {
+                return Json(new { success = false, log = "El campo usuario es obligatorio" }, "application/json");
+            }
+            if (clave == null || clave.Equals(string.Empty))
+            {
+                return Json(new { success = false, log = "El campo clave es obligatorio" }, "application/json");
+            }
+            if (destinatario == null || destinatario.Equals(string.Empty))
+            {
+                return Json(new { success = false, log = "El campo destinatario es obligatorio" }, "application/json");
+            }
+
+            try
+            {
+
+                var fromAddress = new MailAddress(usuario);
+                var toAddress = new MailAddress(destinatario);
+                var fromPassword = clave;
+                const string subject = "Prueba DomiCheckEmail";
+                const string body = "Esto es una prueba de funcionamiento de correo.";
+
+                var smtp = new SmtpClient
+                               {
+                                   Host = servidor,
+                                   Port = int.Parse(puerto),
+                                   EnableSsl = false,
+                                   DeliveryMethod = SmtpDeliveryMethod.Network,
+                                   UseDefaultCredentials = false,
+                                   Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                               };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                                         {
+                                             Subject = subject,
+                                             Body = body
+                                         })
+                {
+                    smtp.Send(message);
+                }
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = true, log = ex.Message }, "application/json");
+            }
+
+            return Json(new { success = true, log = "Mensaje enviado satisfactoriamente" }, "application/json");
+        }
+    }
+}
---
+++ DomiCheckEmail/Models/HomeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DomiCheckEmail.Models
+{
+    public class HomeModel
+    {
+        public string Servidor { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Clave { get; set; }
+
+        public string Destinatario { get; set; }
+
+        public string Logs { get; set; }
+
+        public string Puerto { get; set; }
+    }
+}
